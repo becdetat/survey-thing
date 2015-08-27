13d2ddb9de124ddd938328fcba3cb030fd2f01e2
Ben Scott <ben@belfryimages.com.au>
2015-08-27 10:40:54 +1000

    static nancy host

---
+++ src/SurveyThing/Bootstrapper.cs
@@ -0,0 +1,16 @@
+﻿using Nancy;
+using Nancy.Conventions;
+
+namespace SurveyThing
+{
+    public class Bootstrapper : DefaultNancyBootstrapper
+    {
+        protected override void ConfigureConventions(NancyConventions nancyConventions)
+        {
+            Conventions.StaticContentsConventions.Clear();
+            Conventions.StaticContentsConventions.AddDirectory("/", "app");
+
+            base.ConfigureConventions(nancyConventions);
+        }
+    }
+}
---
+++ src/SurveyThing/Startup.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.Owin.Extensions;
+using Nancy.Owin;
+using Owin;
+
+namespace SurveyThing
+{
+    public class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            app
+                .UseNancy(new NancyOptions
+                {
+                    Bootstrapper = new Bootstrapper()
+                })
+                .UseStageMarker(PipelineStage.MapHandler);
+        }
+    }
+}
